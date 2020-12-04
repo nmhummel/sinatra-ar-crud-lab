6f58f8114bd14d786ea77ecb9744a2bddbb2fe98
nmhummel <natalie.hummel@gmail.com>
2020-12-03 21:12:05 -0600

    commit tree 0408320834d1bb9cecc2dafa320c71f4246c8dd0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +8,58 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  # The Read CRUD action corresponds to two different controller actions: show and index. 
+  # The show action should render the ERB view show.erb, which shows an individual article. 
+  # The index action should render the ERB view index.erb, which shows a list of all of the articles.
+
+  # INDEX
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # NEW ARTICLE
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  # CREATE NEW ARTICLE
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  # SHOW ONE ARTICLE
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # EDIT AN ARTICLE
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # UPDATE ARTICLE
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  # DESTROY ARTICLE
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+  
+  get '/edit' do
+    "Hello World"
+  end
+  
 end
--- config.ru
+++ config.ru
@@ -5,6 +5,7 @@ begin
   fi_check_migration
 
   use Rack::MethodOverride
+  # so that your app will know how to handle PATCH, PUT, and DELETE requests!
 
   run ApplicationController
 rescue ActiveRecord::PendingMigrationError => err
